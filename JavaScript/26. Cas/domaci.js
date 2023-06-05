0552c8a629cf815e633d58336e46f9ee950bb30d
NedimHuseinovic <nedimhuseinovic@gmail.com>
2023-06-05 20:00:06 +0200

    Figma part 1 index

---
+++ JavaScript/26. Cas/domaci.js
@@ -0,0 +1,25 @@
+// const first = () =>{
+//     const a = +prompt ("Unesite prvu potencijalnu stranicu pravougaonika")
+//     const b = +prompt ("Unesite drugu potencijalnu stranicu pravougaonika")
+//     const c = +prompt ("Unesite treci potencijalnu stranicu pravougaonika")
+//     const d = +prompt ("Unesite cetvrtu potencijalnu stranicu pravougaonika")
+//     if(
+//         (a === b === c === d) ||
+//         (a === b && c === d) ||
+//         (a === c && b === d) ||
+//         (a === d && b === c)
+//     ){
+//         return "Moguce je formirati pravougaonik"
+//     } else {
+//         return "Nije moguce formirati pravougaonik"
+//     }
+// }
+// console.log(first())
+
+
+// 2.
+const second = () =>{
+    const brojSekundi = +prompt("Unesite broj sekundi: ")
+    const sati = brojSekundi / 3600
+    
+}
