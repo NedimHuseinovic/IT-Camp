21989d7ef2a5788eb64d36251b81abc43e608e61
NedimHuseinovic <nedimhuseinovic@gmail.com>
2023-06-06 22:32:22 +0200

    Domaci

---
+++ JavaScript/27. Cas/domaci.js
@@ -0,0 +1,11 @@
+function mToKm (m){
+    let km = m/1000
+    return km.toFixed(2)
+}
+console.log(mToKm(600))
+
+function incToCm (cm){
+    let inc = cm/2.54
+    return inc.toFixed(2)
+}
+console.log(incToCm(34))
